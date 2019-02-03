93e814835b530dcfd30d3aa695cf6cdbf5c2d1af
ashraf1994 <ashraf515139@gmail.com>
2019-02-03 12:34:42 +0530

    fisrt commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, hashHistory } from 'react-router-dom';
+import { Provider } from "react-redux";
+// import store from "./store/index";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+// Import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(
+    // <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        // </Provider>
+    , document.getElementById('root'));
+
+
 serviceWorker.unregister();
+
+
+
+
+
